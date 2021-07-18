37faba45600a584f91241cc68d625a1a14053754
michaelgathara <mikegtrm@gmail.com>
2021-07-17 22:53:13 -0500

    coins

---
+++ Project Retain Alabama/Class/normalDistribution.py
@@ -0,0 +1,8 @@
+# import numpy as np
+# import scipy.stats import norm
+# import matplotlib.pyplot as plt
+
+# data = []
+# mu, std = norm.fit(data)
+
+# plt.hist(data, bins=, density=True, alpha=0.5, color='b')
