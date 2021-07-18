33630b7fca0dc99c5955b9144c36e1c712e01d8a
michaelgathara <mikegtrm@gmail.com>
2021-07-18 00:55:33 -0500

    Normal Distribution

--- Project Retain Alabama/Class/normalDistribution.py
+++ Project Retain Alabama/Class/normalDistribution.py
@@ -1,8 +1,24 @@
-# import numpy as np
-# import scipy.stats import norm
-# import matplotlib.pyplot as plt
+# %%
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import pandas as pd 
 
-# data = []
-# mu, std = norm.fit(data)
+data = [[0,1,1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0,1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1], [0,1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1 ,1, 1, 0, 1],[0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1],[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0]]
+mu, std = norm.fit(data)
 
-# plt.hist(data, bins=, density=True, alpha=0.5, color='b')
+plt.hist(data, bins=4, density=True, alpha=0.5, color=['r','b','g','y'])
+# r = Michael Hurt
+# b = Michael Gathara
+# g = Jeremy Meeder
+# y = Tanner Tavel
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Fit results: mu = %.2f, std = %.2f" % (mu, std)
+plt.title(title)
+plt.show()
+
+
+# %%
