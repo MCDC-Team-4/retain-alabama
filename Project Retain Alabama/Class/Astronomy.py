7da9919e0496c78c9e1133d7ef9992fff6b78af9
michaelgathara <mikegtrm@gmail.com>
2021-07-30 16:21:59 -0500

    yeeT

--- Project Retain Alabama/Class/Astronomy.py
+++ Project Retain Alabama/Class/Astronomy.py
@@ -3,18 +3,18 @@ from pylab import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_range = y_range = np.arange(-20,20,5)
+x_range = y_range = np.arange(-40,40,5).reshape(8,2)
 
 xval, yval = np.meshgrid(x_range, y_range)
 
-G = 6.67408E-11    # gravitation constant
+G = 6    # gravitation constant
 m1 = 597000     # Mass of obj 1 in kg
 m2 = 59700    # Mass of obj 2 in kg          feel free to change these
-r = 37900.35  ** 2 # distance b/n masses
+r = 37900  ** 2 # distance b/n masses
     
 
-xdot = yval
-ydot = G * (m1*m2/r)        # Newtons law of Grav
+xdot = xval - xval * yval
+ydot = G * (m1*m2/r)     # Newtons law of Grav
 
 Q = plt.quiver(xval, yval, xdot, ydot, pivot="middle")
 plt.quiverkey(Q,1,1,1, "=Planet")
